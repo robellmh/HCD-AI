d923fb23364a0ae60969a4eecc0580dc87f587d4
LMH-L-7092\ralazar <ralazar@lastmilehealth.org>
2024-11-01 16:37:03 +0300

    up and run on windows

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,5 +1,9 @@
-#!/bin/bash
+# #!/bin/bash
+# python -m alembic upgrade head
+# exec gunicorn -k main.Worker -w 4 -b 0.0.0.0:8000 --preload \
+#     -c gunicorn_hooks_config.py main:app
+# #
 python -m alembic upgrade head
-exec gunicorn -k main.Worker -w 4 -b 0.0.0.0:8000 --preload \
-    -c gunicorn_hooks_config.py main:app
-#
+
+# Start Uvicorn server
+uvicorn main:app --host 0.0.0.0 --port 8000 --workers 4 --reload
