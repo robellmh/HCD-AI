1366589e22d5da9fa61daaa3c4ea914ca3db3698
Mark Botterill <markbotters@gmail.com>
2024-11-22 14:12:28 +0300

    Remove passlib only use bcrypt in utils

--- backend/app/users/utils.py
+++ backend/app/users/utils.py
@@ -1,17 +1,19 @@
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+import bcrypt
 
 
 async def hash_password(password: str) -> str:
     """
     Hash a password using bcrypt.
     """
-    return pwd_context.hash(password)
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode("utf-8"), salt)
+    return hashed.decode("utf-8")
 
 
 async def verify(plain_password: str, hashed_password: str) -> bool:
     """
     Verify a password against a hashed password.
     """
-    return pwd_context.verify(plain_password, hashed_password)
+    return bcrypt.checkpw(
+        plain_password.encode("utf-8"), hashed_password.encode("utf-8")
+    )
