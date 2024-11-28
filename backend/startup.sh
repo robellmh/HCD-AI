12e9178e55a21d9681afe7783a2d7ac545c53f3a
Mark Botterill <markbotters@gmail.com>
2024-11-28 17:35:08 +0300

    added timeout flag to allow for slower calls

--- backend/startup.sh
+++ backend/startup.sh
@@ -6,6 +6,6 @@
 python -m alembic upgrade head
 
 # Start Uvicorn server
-uvicorn main:app --host 0.0.0.0 --port 8000 --workers 4 --reload
+uvicorn main:app --host 0.0.0.0 --port 8000 --workers 4 --reload --timeout-keep-alive 60
 #exec gunicorn -k main.Worker -w 4 -b 0.0.0.0:8000 --preload \
 #    -c gunicorn_hooks_config.py main:app
